8da7cc3ea895b11936b16389a27d78c92f7d4ed9
Pria Purnama <priapurnama@me.com>
2015-12-22 23:47:33 +0700

    Added console message because.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,6 +1,8 @@
 export class MainController {
   constructor () {
     'ngInject';
+
+    console.log("%cInterested in the code? Check it out here https://github.com/priapurnama/wedding",'background: rgb(41, 126, 226); color: #fff; font: 16px/2 "Helvetica Neue", "Helvetica", Arial, sans-serif; padding: .5em;')
   }
 
 }
